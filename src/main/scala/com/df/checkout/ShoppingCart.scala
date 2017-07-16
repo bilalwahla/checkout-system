9e0f12015753c619bb6369fdb1c9f8a5a08471e7
Bilal Wahla <bilalwahla@gmail.com>
2017-07-16 20:46:35 +0100

    1: Shopping cart implementation. Simple and one implementation hence just did all test cases at once in the last commit.

--- src/main/scala/com/df/checkout/ShoppingCart.scala
+++ src/main/scala/com/df/checkout/ShoppingCart.scala
@@ -9,5 +9,9 @@ object ShoppingCart {
   private val orange = "Orange"
   private val apple = "Apple"
 
-  def checkout(cart: ShoppingCart): BigDecimal = ???
+  def checkout(cart: ShoppingCart): BigDecimal = cart.fruits match {
+    case Nil => 0.00
+    case f :: _ if f == apple || f == orange =>
+      cart.fruits.count(_ == apple) * cart.appleCost + cart.fruits.count(_ == orange) * cart.orangeCost
+  }
 }
